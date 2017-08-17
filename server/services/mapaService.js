0a549d744893104f3e5876e17fd560323cb8866b
wembuwon <wembuwon@gmail.com>
2017-08-17 20:45:42 +0200

    1

--- server/services/mapaService.js
+++ server/services/mapaService.js
@@ -5,9 +5,26 @@ var mongoose = require('mongoose'),
     casilla = mongoose.model('CasillaModelo');
 
 
+class CasillaDAO {
+    constructor() {
+        this.conexion = require('../../config.json');
+        this.mongoose = require('mongoose');
+    }
+    obtener_casillas_mapa() {
+        this.mongoose.connect(this.conexion.conexionbd);
+        casilla = mongoose.model('CasillaModelo');
+        casilla.find({}, function(err, casilla) {
+            if (err)
+                return err;
+            return casilla;
+        });
+        mongoose.disconnect();
+    }
+}
+  
 
 exports.getCasillas = function(req, res) {
-  mapaDao = new casillaDao.CasillaDAO();
+  mapaDao = new CasillaDAO();
   var casillas = mapaDao.obtener_casillas_mapa();
     if (err)
       res.send(err);
